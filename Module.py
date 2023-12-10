e05e67094520ac61670256ac5cc11f6a3a3ab331
HunterM284 <112110578+HunterM284@users.noreply.github.com>
2023-12-10 14:44:21 -0500

    Added file conversion to Module.py
    Added file converter to check if file is .wav, should work

--- Module.py
+++ Module.py
@@ -1,8 +1,19 @@
 # Module.py
 import wave
 import numpy as np
+from pydub import AudioSegment
+
 
 def analyze_wav(file_path):
+    # Check if the file is a .wav file
+    if not file_path.lower().endswith('.wav'):
+        converted_file_path = convert_to_wav(file_path)
+        if converted_file_path:
+            file_path = converted_file_path
+        else:
+            print("Error converting file to .wav.")
+            return None, None
+
     with wave.open(file_path, 'rb') as wav_file:
         framerate = wav_file.getframerate()
         frames = wav_file.getnframes()
@@ -10,6 +21,24 @@ def analyze_wav(file_path):
 
         signal = np.frombuffer(wav_file.readframes(frames), dtype=np.int16)
         frequencies = np.fft.fft(signal)
-        frequencies = np.abs(frequencies)[:len(frequencies)//2]
+        frequencies = np.abs(frequencies)[:len(frequencies) // 2]
 
     return duration, frequencies
+
+
+def convert_to_wav(input_file):
+    try:
+        # Load the audio file using pydub
+        audio = AudioSegment.from_file(input_file)
+
+        # Create a new file path with the .wav extension
+        output_file = os.path.splitext(input_file)[0] + ".wav"
+
+        # Export the audio to the new .wav file
+        audio.export(output_file, format="wav")
+
+        return output_file
+
+    except Exception as e:
+        print(f"Error converting file to .wav: {e}")
+        return None
