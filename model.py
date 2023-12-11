e969e8a6d8b5ce75e34557750312baa7d7af268b
JarLane <151280880+JarLane@users.noreply.github.com>
2023-12-10 19:55:48 -0500

    The conversion works now and r60 combined added

---
+++ model.py
@@ -0,0 +1,58 @@
+import wave
+
+import inline
+import matplotlib
+import np
+import numpy as np
+from numpy import argmin
+from pydub import AudioSegment
+import os
+
+
+def analyze_wav(file_path):
+    # Check if the file is a .wav file
+    if not file_path.lower().endswith('.wav'):
+        converted_file_path = convert_to_wav(file_path)
+        if converted_file_path:
+            file_path = converted_file_path
+        else:
+            print("Error converting file to .wav.")
+            return None, None, None
+
+    with wave.open(file_path, 'rb') as wav_file:
+        framerate = wav_file.getframerate()
+        frames = wav_file.getnframes()
+        duration = frames / float(framerate)
+
+        signal = np.frombuffer(wav_file.readframes(frames), dtype=np.int16)
+        frequencies = np.fft.fft(signal)
+        frequencies = np.abs(frequencies)[:len(frequencies) // 2]
+
+        # Split frequencies into low, mid, and high sections
+        low_freq_cutoff = 100  # Adjust as needed
+        high_freq_cutoff = 1000  # Adjust as needed
+
+        low_frequencies = frequencies[:low_freq_cutoff]
+        mid_frequencies = frequencies[low_freq_cutoff:high_freq_cutoff]
+        high_frequencies = frequencies[high_freq_cutoff:]
+
+    return duration, low_frequencies, mid_frequencies, high_frequencies
+
+
+def convert_to_wav(input_file):
+    try:
+        # Load the audio file using pydub
+        audio = AudioSegment.from_file(input_file)
+
+        # Create a new file path with the .wav extension
+        output_file = os.path.splitext(input_file)[0] + ".wav"
+
+        # Export the audio to the new .wav file
+        audio.export(output_file, format="wav")
+
+        return output_file
+
+    except Exception as e:
+        print(f"Error converting file to .wav: {e}")
+        return None
+
--- view.py
+++ view.py
@@ -3,8 +3,12 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from model import analyze_wav
+from model import convert_to_wav
+import numpy as np
 import matplotlib.pyplot as plt
-from Module import analyze_wav
+from scipy.io import wavfile
+
 
 class GUI:
     def __init__(self, master):
@@ -27,6 +31,8 @@ class GUI:
         self.file_label = tk.Label(self.master, text="No file selected")
         self.file_label.pack(pady=5)
 
+        # File name label
+
         # Upload button
         self.upload_button = tk.Button(self.master, text="Upload", state="disabled", command=self.upload_file)
         self.upload_button.pack(pady=10)
@@ -41,6 +47,12 @@ class GUI:
         self.plot_button_high = tk.Button(self.master, text="High", command=self.plot_button_clicked_high)
         self.plot_button_high.pack(side="left", padx=5)
 
+        self.plot_button_all = tk.Button(self.master, text="All", command=self.plot_button_clicked_all)
+        self.plot_button_all.pack(side="left", padx=5)
+
+        self.plot_button_rt60 = tk.Button(self.master, text="RT60", command=self.plot_button_clicked_rt60)
+        self.plot_button_rt60.pack(side="right", padx=5)
+
     def import_file(self):
         self.file_path = filedialog.askopenfilename(
             title="Select a file",
@@ -50,9 +62,9 @@ class GUI:
         if self.file_path:
             # Check if the file is a .wav file
             if not self.file_path.lower().endswith('.wav'):
-                converted_file_path = self.convert_to_wav(self.file_path)
-                if converted_file_path:
-                    self.file_path = converted_file_path
+                self.converted_file_path = convert_to_wav(self.file_path)
+                if self.converted_file_path:
+                    self.file_path = self.converted_file_path
                 else:
                     print("Error converting file to .wav.")
                     return None
@@ -66,6 +78,8 @@ class GUI:
         self.analysis_data = analyze_wav(self.file_path)
         if self.analysis_data is not None:
             duration, _, _, _ = self.analysis_data
+            self.time = tk.Label(self.master, text=f"Time is {duration}")
+            self.time.pack(pady=5)
             print(f"Duration of the audio: {duration:.2f} seconds")
 
     def plot_button_clicked_low(self):
@@ -82,6 +96,22 @@ class GUI:
         if self.analysis_data is not None:
             _, _, _, high_frequencies = self.analysis_data
             self.plot_data(range(len(high_frequencies)), high_frequencies, "High Frequencies", "Frequency (Hz)", "Amplitude", "High Plot", "top")
+
+    def plot_button_clicked_all(self):
+        if self.analysis_data is not None:
+            _, low_frequencies, mid_frequencies, high_frequencies = self.analysis_data
+            self.plot_data_all(
+                range(len(high_frequencies)),
+                high_frequencies,
+                range(len(mid_frequencies)),
+                mid_frequencies,
+                range(len(low_frequencies)),
+                low_frequencies,
+                "All Frequencies",
+                "Frequency (Hz)",
+                "Amplitude",
+                "top"
+            )
 
     def plot_data(self, x_values, y_values, title, xlabel, ylabel, plot_title, side):
         # Remove the current plot container if it exists
@@ -103,10 +133,104 @@ class GUI:
         canvas_widget = canvas.get_tk_widget()
         canvas_widget.pack(side="top", fill="both", expand=1)
 
-# ... (rest of the code remains unchanged)
+    def plot_data_all(self, x_values_high, y_values_high, x_values_mid, y_values_mid, x_values_low, y_values_low, title,
+                      xlabel, ylabel, side):
+        if self.current_plot_container:
+            self.current_plot_container.destroy()
+        fig, ax = plt.subplots()
+        ax.plot(x_values_high, y_values_high, color='r', label='high')
+        ax.plot(x_values_mid, y_values_mid, color='b', label='mid')
+        ax.plot(x_values_low, y_values_low, color='g', label='low')
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        plt.legend()
+
+        # To load the display window
+        self.current_plot_container = tk.Frame(self.master)
+        self.current_plot_container.pack(side=side, fill="both", expand=1)
+
+        canvas = FigureCanvasTkAgg(fig, master=self.current_plot_container)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side="top", fill="both", expand=1)
+    # ... (rest of the code remains unchanged)
+    def plot_button_clicked_rt60(self):
+        if self.current_plot_container:
+            self.current_plot_container.destroy()
+
+        sample_rate, data = wavfile.read(self.file_path)
+        spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        # prints var outputs
+        def debugg(fstring):
+            print(fstring) # comment out for prod
+        # pass
+        def find_target_frequency(freqs):
+            for x in freqs:
+                if x > 1000:
+                    break
+            return x
+        def frequency_check():
+        # you can choose a frequency which you want to check
+            debugg (f'freqs {freqs[:10]}]')
+            target_frequency = find_target_frequency(freqs)
+            debugg (f'target_frequency {target_frequency}')
+            index_of_frequency = np.where(freqs== target_frequency) [0] [0]
+            debugg (f'index_of_frequncy {index_of_frequency}') # find a sound data for a particular frequency
+            data_for_frequency = spectrum [index_of_frequency]
+            debugg (f'data_for_frequency {data_for_frequency [:10]}')
+            # change a digital signal for a values in decibels
+            data_in_db_fun = 10 * np.log10(data_for_frequency)
+            return data_in_db_fun
+        data_in_db = frequency_check()
+        plt.figure()
+        # plot reverb time on grid
+        plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color='#004bc6')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Power (dB)')
+        # find a index of a max value
+        index_of_max= np.argmax(data_in_db)
+        value_of_max = data_in_db[index_of_max]
+        plt.plot(t[index_of_max], data_in_db [index_of_max], 'go')
+        # slice array from a max value
+        sliced_array = data_in_db [index_of_max:]
+        value_of_max_less_5 = value_of_max - 5
+        # find a nearest value
+        def find_nearest_value(array, value):
+            array = np.asarray(array)
+            debugg(f'array {array[:10]}')
+            idx = (np.abs (array - value)).argmin()
+            debugg(f'idx {idx}')
+            debugg (f'array[idx] {array[idx]}')
+            return array[idx]
+        value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+        index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+        plt.plot(t[index_of_max_less_5], data_in_db [index_of_max_less_5], 'yo')
+        # slice array from a max -5dB
+        value_of_max_less_25 = value_of_max - 25
+        value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+        index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+        plt.plot(t[index_of_max_less_25], data_in_db [index_of_max_less_25], 'ro')
+        rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])[0]
+        # extrapolate rt20 to rt60
+        rt60 = 3 * rt20
+        # optional set limits on plot
+        #plt.xlim(0, ((round(abs(rt60), 2)) * 1.5))
+
+
+        plt.grid()  # show grid
+        plt.show() # show plots
+        print (f'The RT60 reverb time is {(rt60)} seconds')
+        self.current_plot_container = tk.Frame(self.master)
+        self.current_plot_container.pack(side="top", fill="both", expand=1)
+
+        canvas = FigureCanvasTkAgg(fig, master=self.current_plot_container)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side="top", fill="both", expand=1)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     gui = GUI(root)
     root.mainloop()
-
